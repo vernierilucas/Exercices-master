0f79bfb0f0af4163a72013c33ee85565bf2720b9
Cadu423 <carlitos.eduards.cz@gmail.com>
2023-10-26 18:55:16 -0300

    initial commit

---
+++ src/config/expressConfig.ts
@@ -0,0 +1,63 @@
+import 'express-async-errors';
+
+import express, { Express } from "express";
+import swaggerUi from "swagger-ui-express";
+import * as swaggerDocument from "../../swagger.json";
+
+import path from "path";
+
+import cors from "cors";
+
+import exerciceRoutes from "../routes/exercice/index";
+import authRoutes from "../routes/auth/index";
+
+import verifyPerm from "../middleware/verifyPerm"
+import errorHandler from "../Errors/errorHandler";
+import validateJWT from '../middleware/validateJWT';
+
+export default function expressConfig(): Express {
+    const app = express();
+    //Configure to use JSON and URLENCODED
+    app.use(cors(
+        {
+            origin: '*'
+        }
+    ));
+
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+
+    //Configure routes
+    app.use(express.static("/home/trilogic/Downloads/Exercices-master/frontend"))
+    app.get("/", (req, res) => {
+        res.sendFile("/home/trilogic/Downloads/Exercices-master/frontend/index.html")
+    })
+    app.get("/login", (req, res) => {
+        res.sendFile("/home/trilogic/Downloads/Exercices-master/frontend/auth/login/index.html")
+    })
+    app.get("/signup", (req, res) => {
+        res.sendFile("/home/trilogic/Downloads/Exercices-master/frontend/auth/signup/index.html")
+    })
+    
+    app.use("/auth", authRoutes.signUpRoute)
+    app.use("/auth", authRoutes.loginRoute)
+    app.use("/auth", authRoutes.checkTokenRoute)    
+
+    app.use(validateJWT)
+    app.use(verifyPerm)
+
+    app.use("/exercice", exerciceRoutes.updateExerciceRoute)
+    app.use("/exercice", exerciceRoutes.addExerciceRoute)
+    app.use("/exercice", exerciceRoutes.submitExerciceRoute)
+    app.use("/exercice", exerciceRoutes.getExerciceRoute)
+    app.use("/exercice", exerciceRoutes.deleteExerciceRoute)
+    app.use("/exercice", exerciceRoutes.listExerciceRoute)
+
+    app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+    app.use(express.static(path.join(__dirname,'public')))
+
+    app.use(errorHandler);
+
+    return app
+}
